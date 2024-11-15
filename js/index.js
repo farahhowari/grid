75f9563d34de0caca5012e770e8b441028c94d57
farahhowari <farahhowari783@gmail.com>
2024-11-16 00:23:19 +0300

    if statment

---
+++ js/index.js
@@ -0,0 +1,42 @@
+console.log("hi im js ")
+var VarName = "ahmad"
+console.log(VarName)
+var number1=3
+var number2=6
+if (number1>number2) {
+    console.log("right")
+}
+else{
+    console.log("false") 
+}
+var choice=5;
+/*if (choice===1) {
+    console.log("cofee")
+    
+}
+else if(choice ==== 2){
+    console.log("water")
+}
+else if(choice ==== 3){
+    console.log("tea")
+}
+    else if(choice ==== 4){
+        console.log("juice")
+    }*/
+   switch(choice){
+    case 1:
+        console.log("cofee")
+        break;
+        case 2:
+        console.log("water")
+        break;
+        case 3:
+        console.log("tea")
+        break;
+        case 4 : 
+        console.log("juice")
+        break;
+        default:
+            console.log("please choose ")
+            break;
+   }
